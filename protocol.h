173424935eaa9e445b692a70a84d1c818f1c0244
znffna <znffna@gmail.com>
2025-01-03 00:12:13 +0900

    Server : IOCP 서버 기본 프레임워크
    IOCP를 통한 서버 생성 및 작업 스레드의 기초에 대해 주석 작성

---
+++ protocol.h
@@ -0,0 +1,16 @@
+constexpr int PORT_NUM = 4000;
+constexpr int BUF_SIZE = 200;
+constexpr int NAME_SIZE = 20;
+
+constexpr int MAX_USER = 5000;
+
+constexpr int W_WIDTH = 400;
+constexpr int W_HEIGHT = 400;
+
+// Packet ID 정의 (Packet ID Definition)
+
+#pragma pack (push, 1)
+
+// 패킷 정의 (Packet Definition)
+
+#pragma pack (pop)
