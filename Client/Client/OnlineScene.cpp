60983db29003cad3548444f820948e2022d99e35
znffna <znffna@gmail.com>
2025-05-12 10:29:19 +0900

    Client : 다른 플레이어 총기 출력 버그 수정

--- Client/Client/OnlineScene.cpp
+++ Client/Client/OnlineScene.cpp
@@ -163,13 +163,14 @@ void COnlineScene::ProcessPacket(PacketHeader* recv_p)
 		case ObjectType::PLAYER:
 		{
 			// 플레이어 오브젝트 추가
-			std::shared_ptr<CPlayer> pPlayer = GetPlayer(); // GetPlayer(skin_type)로 바꿔야 함
+			std::shared_ptr<CPlayer> pPlayer = GetPlayer(packet->fixdata.skin_type); // GetPlayer(skin_type)로 바꿔야 함
 			pPlayer->SetPosition(packet->fixdata.startposition.x, packet->fixdata.startposition.y, packet->fixdata.startposition.z);
 			m_mapGameObjects[packet->id] = pPlayer;
 
 			int gun_type = packet->fixdata.gun_type;
 			std::shared_ptr<CGun> pGun = CGun::Create(nullptr, nullptr, nullptr, gun_type);
 			pPlayer->SetGun(pGun);
+			AddObject(pGun);
 
 			{
 				std::string DebugOutput = "ObjectType::PLAYER 생성 완료\n";
@@ -210,6 +211,7 @@ void COnlineScene::ProcessPacket(PacketHeader* recv_p)
 		Vec3 look = updatePkt->obj.meta.look;
 		m_mapGameObjects[updatePkt->id]->SetLook(look.x, look.y, look.z);
 		m_mapGameObjects[updatePkt->id]->SetPosition(position.x, position.y, position.z);
+		updatePkt->obj.act_type; // State
 
 	
 		if (g_bNetworkDebugMode) {
